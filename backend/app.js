4772f4d1b7f4f3906b5b2a66e9e74a5bb761eb86
amaanslyf <shaikhabdulshakkur@gmail.com>
2025-08-03 20:07:11 +0530

    initial commit

--- backend/app.js
+++ backend/app.js
@@ -1,31 +1,39 @@
 import express from "express";
 import {createServer} from "node:http";
-import {Server} from "socket.io";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 
+import userRoutes from "./routes/user.routes.js";
 import {connectToSocket} from "./controllers/socketManager.js";
 
+dotenv.config();
+
 const app = express();
-const server = createServer(app);    // creating http server of express 
-const io = connectToSocket(server);       // creating socket server of http server
+const server = createServer(app);
+const io = connectToSocket(server);
 
-app.set("port",(process.env.PORT || 8000));  
-app.use(cors());  // for cors
-app.use(express.json({limit:"40kb"}));  // for parsing application/json
-app.use(express.urlencoded({limit:"40kb",extended:true})); // for parsing application/x-www-form-urlencoded    
+app.set("port",(process.env.PORT || 8000));
+app.use(cors());
+app.use(express.json({limit:"40kb"}));   //The limit option specifies the maximum size of the request body in bytes.
+app.use(express.urlencoded({limit:"40kb",extended:true}));  
 
-const start =async ()=>{
+app.use("/api/v1/users",userRoutes);
 
-    app.set("mongo_user")
-    const connectionDb= await mongoose.connect("mongodb+srv://amaanslyf:Sandhuk%4024@huddleup.u4v3cnd.mongodb.net/?retryWrites=true&w=majority&appName=HuddleUp");
+const start = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Database connected successfully.");
 
-    if(connectionDb){
-        console.log("Database connected");
+        server.listen(app.get("port"), () => {
+            console.log(`Server started on port ${app.get("port")}`);
+        });
+
+    } catch (error) {
+        console.error("!!! FAILED TO START SERVER !!!");
+        console.error(error);
+        process.exit(1); 
     }
-    
-    server.listen(app.get("port"), () => {
-        console.log(`Server started on port ${app.get("port")}`);
-    });
 }
+
 start();
