f355ce060a68d9ecff438689e4fb5dd81cd5465a
amaanslyf <shaikhabdulshakkur@gmail.com>
2025-08-03 20:05:25 +0530

    this is mongoose schema model to store users in db

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,14 +1,14 @@
+
 import mongoose, { Schema } from "mongoose";
 
-const userScheme = new Schema(
-    {
-        name: { type: String, required: true },
-        username: { type: String, required: true, unique: true },
-        password: { type: String, required: true },
-        token: { type: String }
-    }
-)
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+  },
+  { timestamps: true }
+);
 
-const User = mongoose.model("User", userScheme);
-
-export { User };
+const User = mongoose.model("User", userSchema);
+export default User;
